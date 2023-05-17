3b29c573e1e92cc56b383cd9c0287448f2961bf3
Warley Dev <warleycm7@gmail.com>
2023-05-17 16:50:30 -0300

    layout da pagina principal de listagem feito, com os cards prontos

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,43 @@
-function Listng(){
-  return(
-    <h1>Página de listagem</h1>
+import MovieCard from "components/MovieScore/MovieCard";
+import Pagination from "components/Pagination";
+
+function Listng() {
+  return (
+    <>
+      <Pagination />
+
+
+      <div className="container">
+
+        <div className="row">
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+        </div>
+
+      </div>
+
+
+    </>
+
   );
 }
 export default Listng;
