0420eeac675549848df34f57dd28b8a5084f2522
Warley Dev <warleycm7@gmail.com>
2023-05-19 20:40:53 -0300

    conexao entre o front e o backend feita, imprimindo na tela os filmes de acordo com o banco de dados

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,34 @@
+import axios from "axios";
 import MovieCard from "components/MovieScore/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 
 function Listng() {
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+      .then(response => {
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
+
   return (
     <>
       <Pagination />
@@ -11,25 +38,12 @@ function Listng() {
 
         <div className="row">
 
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+          {page.content.map(movie =>
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          )}
 
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
 
         </div>
 
